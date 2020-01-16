5e5d647b74cda4c81a047429b5914733c920f08c
qixiaobao <1011189332@qq.com>
2020-01-16 16:02:06 +0800

    12

--- src/script/login.js
+++ src/script/login.js
@@ -1,9 +1,28 @@
-class log{
-    constructor(){
-
+class log {
+    constructor() {
+        this.username = $(".username");
+        this.password = $(".password");
+        this.login = $(".login");
     }
-    init(){
-
+    init() {
+        this.login.on("click", () => {
+            $.ajax({
+                type: "post",
+                url: "http://10.31.152.37/Gree/php/login.php",
+                data: {
+                    user: this.username.val(),
+                    pass: this.password.val()
+                }
+            }).done((result) => {
+                if (result) { //匹配成功
+                    location.href = 'index.html';
+                    localStorage.setItem('username', this.username.val());
+                } else { //匹配失败
+                    this.password.val('');
+                    alert('用户名或者密码错误');
+                }
+            });
+        });
     }
 }
 
@@ -19,11 +38,11 @@ function commonality() {
     $("#footer").load("footer.html");//尾部相同的结构
 }
 
-function login(){
+function login() {
     commonality()
     new log().init();
 }
 
-export{
+export {
     login
 }
